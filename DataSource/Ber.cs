8d1baeb5e94f2799fa0af432d9c71b9acee7edbf
lajtosrob <lajtosrobert@freemail.hu>
2023-11-20 15:56:15 +0100

    Add project files.

---
+++ DataSource/Ber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Console_Berek2020.DataSource
+{
+    internal class Ber
+    {
+
+        string nev;
+        string neme;
+        string reszleg;
+        int belepesEve;
+        double fizetes;
+
+        public Ber(string nev, string neme, string reszleg, int belepesEve, double fizetes)
+        {
+            this.nev = nev;
+            this.neme = neme;
+            this.reszleg = reszleg;
+            this.belepesEve = belepesEve;
+            this.fizetes = fizetes;
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+        public string Neme { get => neme; set => neme = value; }
+        public string Reszleg { get => reszleg; set => reszleg = value; }
+        public int BelepesEve { get => belepesEve; set => belepesEve = value; }
+        public double Fizetes { get => fizetes; set => fizetes = value; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using Console_Berek2020.DataSource;
+
+namespace Console_Berek2020
+{
+    internal class Program
+    {
+        static List<Ber> berek = new List<Ber>();
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(".\\DataSource\\berek2020.txt");
+
+            sr.ReadLine(); // fejlécsor beolvasása
+
+            while (!sr.EndOfStream) 
+            {
+                string[] line = sr.ReadLine().Split(";");
+
+                Ber adatsor = new Ber(
+                    line[0],
+                    line[1],
+                    line[2],
+                    int.Parse(line[3]),
+                    double.Parse(line[4])
+                    );
+
+                berek.Add(adatsor);
+
+            }
+
+            sr.Close();
+
+            // 3. feladat
+
+            Console.WriteLine($"3. feladat: Dolgozók száma: {berek.Count()} fő");
+
+            // 4. feladat
+
+            Console.WriteLine($"4. feladat: Bérek átlaga: {Math.Round(berek.Average(x => x.Fizetes/1000),1)} eFt");
+
+            // 5. feladat 
+
+            Console.Write("Kérem egy részleg nevét: ");
+            string reszlegNeve = Console.ReadLine();
+
+            // 6. feladat
+
+
+
+        }
+    }
+}
